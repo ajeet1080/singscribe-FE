094e5030fb31ac48376ae4028b5fbd6b5898bcdf
Ajeet <08.ajeet@gmail.com>
2024-02-04 23:37:16 +0800

    Updated api poiniting to react

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ import {
   Stack,
   Radio,
 } from "@chakra-ui/react";
-import axios from "axios";
+
 import shslogo from "./assets/singhealth-logo.png";
 import shsdividerlogo from "./assets/shs-divider.png";
 import {
@@ -33,16 +33,14 @@ import {
   SpeechConfig,
   AudioConfig,
 } from "microsoft-cognitiveservices-speech-sdk";
+
 useColorModeValue;
 
 const App: React.FC = () => {
   const [recording, setRecording] = useState<boolean>(false);
-  const [transcript, setTranscript] = useState<string>("");
   const [summary, setSummary] = useState<string>(""); // State for the summary text
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const toast = useToast();
-  const [apiResponseReceived, setApiResponseReceived] =
-    useState<boolean>(false);
   const [isLoadingTranscript, setIsLoadingTranscript] =
     useState<boolean>(false);
   const [isLoadingSummary, setIsLoadingSummary] = useState<boolean>(false);
@@ -56,6 +54,15 @@ const App: React.FC = () => {
   const [language, setLanguage] = useState("en-SG");
   const [isOpen, setIsOpen] = useState(true);
   const onClose = () => setIsOpen(false);
+  const [isEditing, setIsEditing] = useState(false); // State for the editing mode
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
 
   const toggleRecording = async () => {
     if (recording) {
@@ -139,36 +146,178 @@ const App: React.FC = () => {
       .map((t) => `${t.speakerId}: ${t.text}`)
       .join("\n");
     // setIsLoading(true);
-    axios
-      .post(
-        "https://sgs-genai-omr-api.azurewebsites.net/format_transcript",
-        { transcript },
-        { headers: { "Content-Type": "application/json" } }
-      )
-      .then((formatResponse) => {
-        setTranscript(formatResponse.data.content); // Update as per actual response structure
-        setFormattedTranscript(formatResponse.data.content);
-        setIsLoadingTranscript(false);
-        //  setIsLoading(true);
-      });
+    setIsLoading(true);
 
-    axios
-      .post(
-        "https://sgs-genai-omr-api.azurewebsites.net/summarize_transript",
-        { transcript },
-        { headers: { "Content-Type": "application/json" } }
-      )
-      .then((summarizeResponse) => {
-        setSummary(summarizeResponse.data.content); // Update as per actual response structure
-        setIsLoadingSummary(false);
-        // setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://shplayground2.openai.azure.com/openai/deployments/4Base/chat/completions?api-version=2023-07-01-preview",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "api-key": "fefc20d1c3ee4046b446c239f96e4fc4",
+          },
+          body: JSON.stringify({
+            messages: [
+              {
+                role: "system",
+                content: `You will receive a conversation transcript between a doctor and a patient in either English, Mandarin, Indonesian, or Tamil. Your task is to summarize the transcript in English, ensuring that it is easily comprehensible. Please refrain from adding any information not included in the original transcript.
+                  Your summary should incorporate the following sections: Problem, Medical history, Medications, Allergies, Family history, Social history, Physical exam, Assessment, Plan.
+                  Please use HTML markup to structure your summary. Highlight with HTML markup as <b style='color: #4d4d4d; font-weight: bold; font-size: 19px;'>Section Heading </b> tag for each section heading. Make sure there are no syntax errors in your generated HTML markup. Highlight crucial medical information like diagnoses and medicine names using the <b style='color: red; font-weight: bold; font-size: 18px;'> tag.
+                  Follow the section headings and their order provided in the Sample Summary below, starting with the Problem. Ensure there is a line break at the end of each section.
+                  
+                  Sample Summary:
+                  <b> Problem </b> <br/>
+                  The patient presented with a <b>headache<b>.<br/>
+                  <b> Medical history </b> <br/>
+                  The patient has a <b>history of migraines<b>.<br/>
+                  <b> Medications </b> <br/>
+                  The patient is currently taking <b>paracetamol<b>.<br/>
+                  <b> Allergies</b> <br/>
+                  The patient is <b>allergic to penicillin<b>.<br/>
+                  <b> Family history</b> <br/>
+                  The patient's mother has a history of migraines.<br/>
+                  <b> Social history</b> <br/>
+                  The patient is a non-smoker.<br/>
+                  <b> Physical exam</b> <br/>
+                  The patient's blood pressure is 120/80.<br/>
+                  <b> Assessment</b> <br/>
+                  The patient has a <b> migraine <b> <br/>
+                  <b> Plan </b> <br/>
+                  The patient is to <b> take paracetamol <b> and see doctor in 2 weeks. <br/>`,
+              },
+              {
+                role: "assistant",
+                content: transcript,
+              },
+            ],
+            temperature: 0.2,
+            top_p: 1,
+            max_tokens: 1000,
+            stream: true,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      if (response.body) {
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder("utf-8");
+        while (true) {
+          const chunk = await reader.read();
+          const { done, value } = chunk;
+          if (done) {
+            break;
+          }
+          const decodedChunk = decoder.decode(value);
+          const lines = decodedChunk.split("\n");
+          const nonEmptyLines = lines.filter(
+            (line) => line !== "" && line !== "[DONE]"
+          );
+          const parsedLines = nonEmptyLines
+            .filter((line) => {
+              try {
+                JSON.parse(line.replace(/^data: /, ""));
+                return true;
+              } catch {
+                return false;
+              }
+            })
+            .map((line) => JSON.parse(line.replace(/^data: /, "")));
+          for (const parsedLine of parsedLines) {
+            const { choices } = parsedLine;
+
+            if (choices && choices.length > 0) {
+              const { delta } = choices[0];
+
+              if (delta) {
+                setSummary((currentSummary) =>
+                  currentSummary
+                    ? `${currentSummary}${delta.content}`
+                    : delta.content
+                );
+                setIsLoadingSummary(false);
+              }
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
+    try {
+      const response = await fetch(
+        "https://shplayground2.openai.azure.com/openai/deployments/4Base/chat/completions?api-version=2023-07-01-preview",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "api-key": "fefc20d1c3ee4046b446c239f96e4fc4",
+          },
+          body: JSON.stringify({
+            messages: [
+              {
+                role: "system",
+                content:
+                  "You will be provided with a transcript of a conversation between a doctor and a patient in either of English, Mandarin, Indonesian or Tamil language. You need to reformat the transcript in English in a way that it is easy to read and understand. Please ensure to do proper tagging as Doctor, Patient. You can use any format provided in Sample Transcript below. Do not add any additional information to the transcript. \n\nSample Transcript:\nDoctor: Hello, how are you?\nPatient: I am fine, thank you.\nDoctor: What brings you here today?\nPatient: I have a headache.\nDoctor: How long have you had it?\nPatient: For about a week.",
+              },
+              {
+                role: "assistant",
+                content: transcript,
+              },
+            ],
+            temperature: 0.2,
+            top_p: 1,
+            max_tokens: 2500,
+            stream: false,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = await response.json();
+      setFormattedTranscript(result.choices[0].message.content);
+      setIsLoadingTranscript(false);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleCopy = async () => {
+    try {
+      // Remove HTML tags from the summary before copying
+      const plainTextSummary = summary.replace(/<[^>]+>/g, "");
+      await navigator.clipboard.writeText(plainTextSummary);
+      toast({
+        title: "Summary copied",
+        description: "The summary has been copied to clipboard.",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
       });
-    setApiResponseReceived(true);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+      toast({
+        title: "Copy failed",
+        description: "Failed to copy the summary to clipboard.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
   };
 
   return (
     <VStack
-      spacing={4}
+      spacing={2}
       p={4}
       backgroundColor="gray.100"
       style={{ minHeight: "100vh" }}
@@ -202,10 +351,10 @@ const App: React.FC = () => {
         textAlign="center"
         borderRadius="lg" // rounded corners
         boxShadow="lg" // shadow effect
-        p={5} // padding
+        p={1} // padding
         bg="white" // white background
         w="70%" // limit width
-        margin="auto" // center the card
+        // center the card
       >
         <img src={shslogo} alt="SingHealth Logo" width={225} height={125} />
         <Text fontSize="4xl" fontWeight="bold" color="#E54809" align="center">
@@ -232,13 +381,13 @@ const App: React.FC = () => {
           <Tabs
             flex="1"
             width="100%"
-            height={500}
+            height={600}
             p={3}
             borderRadius="md"
             boxShadow="lg"
             bg="white"
           >
-            <Text fontSize="2xl" fontWeight="bold">
+            <Text fontSize="2xl" fontWeight="bold" color="#E54809">
               Transcript
             </Text>
             <TabList>
@@ -250,7 +399,13 @@ const App: React.FC = () => {
               </Tab>
             </TabList>
             <TabPanels>
-              <TabPanel width="100%" height={450} p={3} borderRadius="md">
+              <TabPanel
+                width="100%"
+                height={475}
+                p={3}
+                borderRadius="md"
+                overflow="auto"
+              >
                 {transcription.map((t, index) => (
                   <p
                     key={index}
@@ -262,7 +417,13 @@ const App: React.FC = () => {
                   </p>
                 ))}
               </TabPanel>
-              <TabPanel width="100%" height={450} p={3} borderRadius="md">
+              <TabPanel
+                width="100%"
+                height={475}
+                p={3}
+                borderRadius="md"
+                overflow="auto"
+              >
                 {isLoadingTranscript ? (
                   <Spinner
                     label="Formating transcript, please wait..."
@@ -270,19 +431,22 @@ const App: React.FC = () => {
                     color="#E54809"
                   />
                 ) : (
-                  formattedTranscript.split("\n").map((line, index) => {
-                    const [speaker, text] = line.split(": ");
-                    return (
-                      <p
-                        key={index}
-                        style={{
-                          color: speaker === "Doctor" ? "blue" : "red",
-                        }}
-                      >
-                        {speaker} : {text}
-                      </p>
-                    );
-                  })
+                  formattedTranscript
+                    .split("\n")
+                    .filter((line) => line.trim() !== ":")
+                    .map((line, index) => {
+                      const [speaker, text] = line.split(": ");
+                      return (
+                        <p
+                          key={index}
+                          style={{
+                            color: speaker === "Doctor" ? "blue" : "red",
+                          }}
+                        >
+                          {speaker} : {text}
+                        </p>
+                      );
+                    })
                 )}
               </TabPanel>
             </TabPanels>
@@ -290,25 +454,49 @@ const App: React.FC = () => {
           <Box
             flex="1"
             width="100%"
-            height={500}
+            height={600}
             p={3}
             borderRadius="md"
             boxShadow="lg"
             bg="white"
           >
-            <Text fontSize="2xl" fontWeight="bold">
+            <Text fontSize="2xl" fontWeight="bold" color="#E54809">
               Summary
             </Text>
+            {isEditing ? (
+              <Button onClick={handleSave} m={2}>
+                Save
+              </Button>
+            ) : (
+              <Button onClick={handleEdit} m={2}>
+                Edit (with HTML)
+              </Button>
+            )}{" "}
+            <Button onClick={handleCopy} m={2}>
+              Copy
+            </Button>
             {isLoadingSummary ? (
               <Spinner size="xl" color="#E54809" />
-            ) : (
+            ) : isEditing ? (
               <Textarea
                 width="100%"
-                height={450}
+                height={475}
                 p={3}
                 borderRadius="md"
                 value={summary}
                 onChange={(e) => setSummary(e.target.value)}
+                overflow={"auto"}
+              />
+            ) : (
+              <Box
+                width="100%"
+                height={475}
+                p={3}
+                borderRadius="md"
+                overflow={"auto"}
+                dangerouslySetInnerHTML={{
+                  __html: summary,
+                }}
               />
             )}
           </Box>
