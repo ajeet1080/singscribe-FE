60f8dcc77c5eb1cde55ef83f914c98557415d884
Ajeet <08.ajeet@gmail.com>
2024-04-20 12:13:29 +0800

    Updated general prompt

--- src/prompts/general.tsx
+++ src/prompts/general.tsx
@@ -4,22 +4,22 @@ const prompt_general = `You will receive a conversation transcript between a doc
                   Follow the section headings and their order provided in the Sample Summary below, starting with the Problem. Ensure there is a line break at the end of each section. Show only those sections from below sample summary for which data input is available.
                   
                   Sample Summary:
-                  <b> Problem </b> <br/>
-                  The patient presented with a <b>headache<b>.<br/>
-                  <b> Medical history </b> <br/>
-                  The patient has a <b>history of migraines<b>.<br/>
-                  <b> Medications </b> <br/>
-                  The patient is currently taking <b>paracetamol<b>.<br/>
-                  <b> Allergies</b> <br/>
-                  The patient is <b>allergic to penicillin<b>.<br/>
-                  <b> Family history</b> <br/>
-                  The patient's mother has a history of migraines.<br/>
-                  <b> Social history</b> <br/>
-                  The patient is a non-smoker.<br/>
-                  <b> Physical exam</b> <br/>
-                  The patient's blood pressure is 120/80.<br/>
-                  <b> Assessment</b> <br/>
-                  The patient has a <b> migraine <b> <br/>
-                  <b> Plan </b> <br/>
-                  The patient is to <b> take paracetamol <b> and see doctor in 2 weeks. <br/>`;
+                  <b> Problem </b> 
+                  The patient presented with a <b>headache<b>.
+                  <b> Medical history </b> 
+                  The patient has a <b>history of migraines<b>.
+                  <b> Medications </b> 
+                  The patient is currently taking <b>paracetamol<b>.
+                  <b> Allergies</b> 
+                  The patient is <b>allergic to penicillin<b>.
+                  <b> Family history</b> 
+                  The patient's mother has a history of migraines.
+                  <b> Social history</b> 
+                  The patient is a non-smoker.
+                  <b> Physical exam</b> 
+                  The patient's blood pressure is 120/80.
+                  <b> Assessment</b> 
+                  The patient has a <b> migraine <b> 
+                  <b> Plan </b> 
+                  The patient is to <b> take paracetamol <b> and see doctor in 2 weeks.`;
 export default prompt_general;
